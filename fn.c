b281b25660aeb5c59d1500dca2f19d4097216481
Pomme <q.bckrt@gmail.com>
2018-08-02 00:16:09 +0200

    Simplified bin_to_dec64, now works from the from string[0].

--- fn.c
+++ fn.c
@@ -5,27 +5,16 @@
 //Convert a binary number (encoded in a string, max 64-bits) in decimal.
 long bin_to_dec64(char bin[])
 {
-	int i = 0, curr = 0;
+	int i = 0;
 	long dec = 0;
-	unsigned long dig = 1;
 
-	//Set 'curr' to the end of the string.
 	while (bin[i] != '\0') {
-		curr = i;
+		dec *= 2;
+		if (bin[i] == '1')
+			dec += 1;
 		i++;
 	}
-	//Convert.
-	while (curr >= 0) {
-		if (bin[curr] == '1') 
-			dec += dig;
-		//Test if bin[] is actually a binary number.
-		if (bin[curr] != '0' && bin[curr] != '1') {
-			printf("Error: Not a binary number.\n");
-			exit(EXIT_FAILURE);
-		}
-		dig *= 2;
-		curr--;
-	}
-
+	
 	return dec;
 }
+
